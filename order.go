92a1c3d38a8b88422bbc2c087889f1f4393dcbf0
ixcoding <ixcoding@hotmail.com>
2024-09-14 16:14:25 +0000

    update for KVer's new api Index, now all index can writer with receiver, kvtparam is more simple

--- order.go
+++ order.go
@@ -19,21 +19,13 @@ type Order struct {
 }
 
 // produce a primary key(pk) from a Order object,
-// save  in the main bucket like that (pk(return by order_pk_ID),  value(return by order_valueEncode))
+// save in the main bucket
 func (this *Order) Key() ([]byte, error) {
 	return kvt.Bytes(kvt.Ptr(&this.ID), unsafe.Sizeof(this.ID)), nil
 }
 
-// a union index function: Type and Status
-// this index function produce a key of idx bucket, and the value is primary key(order_pk_ID produce)
-func order_idx_Type_Status(obj interface{}) ([]byte, error) {
-	c, _ := obj.(*Order)
-	ret := kvt.MakeIndexKey(nil, []byte(c.Type), kvt.Bytes(kvt.Ptr(&c.Status), unsafe.Sizeof(c.Status)))
-	return ret, nil
-}
-
 // encode should match with decode, here we use gob, you can use json as you like
-// generater []byte to save you obj into kv db
+// generater []byte value to save you obj into kv db
 func (this *Order) Value() ([]byte, error) {
 	var network bytes.Buffer // Stand-in for the network.
 	// Create an encoder and send a value.
@@ -42,8 +34,25 @@ func (this *Order) Value() ([]byte, error) {
 	return network.Bytes(), nil
 }
 
+// produce a primary key(pk) from a Order object,
+// save  in the main bucket like that (pk(return by order_pk_ID),  value(return by order_valueEncode))
+func (this *Order) Index(name string) ([]byte, error) {
+	switch name {
+	case "idx_Type_Status":
+		return this.order_idx_Type_Status()
+	}
+	return nil, fmt.Errorf("Index not found")
+}
+
+// a union index function: Type and Status
+// this index function produce a key of idx bucket, and the value is primary key(order_pk_ID produce)
+func (this *Order) order_idx_Type_Status() ([]byte, error) {
+	ret := kvt.MakeIndexKey(nil, []byte(this.Type), kvt.Bytes(kvt.Ptr(&this.Status), unsafe.Sizeof(this.Status)))
+	return ret, nil
+}
+
 // unmarshal []byte to your obj
-func order_valueDecode(b []byte, obj any) (any, error) {
+func order_valueDecode(b []byte, obj kvt.KVer) (kvt.KVer, error) {
 	r := bytes.NewReader(b)
 	dec := gob.NewDecoder(r)
 	var c *Order
@@ -61,7 +70,7 @@ func makeKVT() *kvt.KVT {
 	kp := kvt.KVTParam{
 		Bucket:    "bkt_Order",
 		Unmarshal: order_valueDecode,
-		Indexs: []kvt.Index{
+		Indexs: []kvt.IndexInfo{
 			//self define index, 3 key infos: index name,  index fields and index function
 			//fields is optional, will parse from index name when omitted, fields should match with the struct field name
 			//index name is also optional, will user index function name if ommit index name
@@ -70,11 +79,8 @@ func makeKVT() *kvt.KVT {
 			//index function name is arbitrary, match with index name is a better choice
 			//index can't ommit
 			{
-				&kvt.IndexInfo{
-					Name: "idx_Type_Status",
-					//Fields: []string{"Type", "Status"}, //Fields is optional, when omit, will parse from the index name
-				},
-				order_idx_Type_Status,
+				Name: "idx_Type_Status",
+				//Fields: []string{"Type", "Status"}, //Fields is optional, when omit, will parse from the index name
 			},
 		},
 	}
